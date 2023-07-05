1186db225e4acb2d9b40d0ff0b7b29c5732c31b0
Mydmoov <denizot.myriam@gmail.com>
2023-07-05 14:00:44 +0200

    2/ app lettings

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Letting
-from .models import Address
+from lettings.models import Letting, Addresse
 from .models import Profile
 
 
 admin.site.register(Letting)
-admin.site.register(Address)
+admin.site.register(Addresse)
 admin.site.register(Profile)
