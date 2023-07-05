44c71b142fc48d61936e70040da25348ab7e7b5b
Mydmoov <denizot.myriam@gmail.com>
2023-07-05 16:57:14 +0200

    update with duble tab

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from lettings.models import Letting, Addresse
-from .models import Profile
+from profiles.models import Profile
 
 
 admin.site.register(Letting)
--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinLengthValidator
-from django.contrib.auth.models import User
+# from django.core.validators import MaxValueValidator, MinLengthValidator
+# from django.contrib.auth.models import User
 
 
 # class Addresse(models.Model):
@@ -23,9 +23,9 @@ from django.contrib.auth.models import User
 #         return self.title
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_city = models.CharField(max_length=64, blank=True)
-
-    def __str__(self):
-        return self.user.username
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     favorite_city = models.CharField(max_length=64, blank=True)
+#
+#     def __str__(self):
+#         return self.user.username
