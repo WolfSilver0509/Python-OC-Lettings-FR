9f6e06beeccfdb1f005ab7769bbf90f93b8c7a4c
Mydmoov <denizot.myriam@gmail.com>
2023-07-07 10:54:10 +0200

    refactor app

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from lettings.models import Letting, Addresse
+
+
+
+admin.site.register(Letting)
+admin.site.register(Addresse)
+
--- oc_lettings_site/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from lettings.models import Letting, Addresse
-from profiles.models import Profile
-
-
-admin.site.register(Letting)
-admin.site.register(Addresse)
-admin.site.register(Profile)
